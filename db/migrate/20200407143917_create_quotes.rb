054c3f3962fd1a82da3a78d679c32f84f5cfa11b
Gigi <dergigi@pm.me>
2020-04-07 17:37:22 +0200

    Revert "Remove author from quotes migration"
    This reverts commit 573261e802bb2ca150bc69eeafc4aad1b97a2893.

--- db/migrate/20200407143917_create_quotes.rb
+++ db/migrate/20200407143917_create_quotes.rb
@@ -1,6 +1,7 @@
 class CreateQuotes < ActiveRecord::Migration[6.0]
   def change
     create_table :quotes do |t|
+      t.string :author
       t.text :body
       t.string :source
       t.date :date
