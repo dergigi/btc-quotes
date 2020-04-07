c0cc1c33e07a89af6adb3082b110be03b2a053b0
Gigi <dergigi@pm.me>
2020-04-07 16:59:22 +0200

    [WIP] Load spreadsheet values

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require "google_drive"
+
+session = GoogleDrive::Session.from_service_account_key("client_secret.json")
+ws = session.spreadsheet_by_title("Bitcoin Resources").worksheets[1]
+
+p ws[2,1]
