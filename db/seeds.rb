342bbbec6ef1fbb4967a5b46484905ac6368f11f
Gigi <dergigi@pm.me>
2020-04-07 17:33:27 +0200

    [WIP] Scrape data from google doc

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,12 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 require "google_drive"
+require "date"
 
 session = GoogleDrive::Session.from_service_account_key("client_secret.json")
 ws = session.spreadsheet_by_title("Bitcoin Resources").worksheets[1]
 
-p ws[2,1]
+# ws.num_rows
+(2..5).each do |row| # skip first row (heading)
+  Quote.create(author: ws[row, 1], body: ws[row, 3], source: ws[row, 4], date: Date.parse(ws[row, 5]), audio: ws[row, 6])
+end
