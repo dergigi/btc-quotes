573261e802bb2ca150bc69eeafc4aad1b97a2893
Gigi <dergigi@pm.me>
2020-04-07 17:33:38 +0200

    Remove author from quotes migration

--- db/migrate/20200407143917_create_quotes.rb
+++ db/migrate/20200407143917_create_quotes.rb
@@ -1,7 +1,6 @@
 class CreateQuotes < ActiveRecord::Migration[6.0]
   def change
     create_table :quotes do |t|
-      t.string :author
       t.text :body
       t.string :source
       t.date :date
