006a30f7b8ff45865b6e683f3c5de93e75c60919
Gigi <dergigi@pm.me>
2020-04-14 15:34:04 +0200

    Add explicit 'random' route for quotes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root 'quotes#random'
   resources :authors, only: [:index]
-  resources :quotes, only: [:index, :show]
+  resources :quotes, only: [:index, :show] do
+    collection do
+      get 'random'
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
