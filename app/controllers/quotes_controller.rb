3f1ec39ca5b45b0932a73751a7cf9dacf24b8886
Gigi <dergigi@pm.me>
2020-04-14 11:27:07 +0200

    Remove quotes scaffolding

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,5 +1,5 @@
 class QuotesController < ApplicationController
-  before_action :set_quote, only: [:show, :edit, :update, :destroy]
+  before_action :set_quote, only: [:show]
 
   # GET /quotes
   # GET /quotes.json
@@ -16,55 +16,6 @@ class QuotesController < ApplicationController
   # GET /quotes/1
   # GET /quotes/1.json
   def show
-  end
-
-  # GET /quotes/new
-  def new
-    @quote = Quote.new
-  end
-
-  # GET /quotes/1/edit
-  def edit
-  end
-
-  # POST /quotes
-  # POST /quotes.json
-  def create
-    @quote = Quote.new(quote_params)
-
-    respond_to do |format|
-      if @quote.save
-        format.html { redirect_to @quote, notice: 'Quote was successfully created.' }
-        format.json { render :show, status: :created, location: @quote }
-      else
-        format.html { render :new }
-        format.json { render json: @quote.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /quotes/1
-  # PATCH/PUT /quotes/1.json
-  def update
-    respond_to do |format|
-      if @quote.update(quote_params)
-        format.html { redirect_to @quote, notice: 'Quote was successfully updated.' }
-        format.json { render :show, status: :ok, location: @quote }
-      else
-        format.html { render :edit }
-        format.json { render json: @quote.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /quotes/1
-  # DELETE /quotes/1.json
-  def destroy
-    @quote.destroy
-    respond_to do |format|
-      format.html { redirect_to quotes_url, notice: 'Quote was successfully destroyed.' }
-      format.json { head :no_content }
-    end
   end
 
   private
